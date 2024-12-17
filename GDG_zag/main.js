a20b65be96f3fc57081983b8825d1ebb6a059da8
Ahmed Gouda <167818789+ahmedgouda5@users.noreply.github.com>
2024-12-17 13:17:45 +0200

    Add files via upload

---
+++ GDG_zag/main.js
@@ -0,0 +1,43 @@
+//!! scope
+
+// let Hi = "Hi ahmed";
+
+// function msg() {
+//   let Hi="Hello ahmed"
+//   console.log(Hi);
+// }
+
+// msg();
+
+// let birthyear = 2003;
+// let currrentYear = 2024;
+// let age=20;
+// function age() {
+//   let age = currrentYear - birthyear;
+//   console.log("from age fn " + age);
+
+//   function day() {
+//     let day = age * 360;
+//     console.log("from day fn " + day);
+//   }
+//   day()
+// }
+
+// age()
+
+// ~ Hoisting
+
+// function sayName() {
+//   console.log(name);
+//   var name = "ahmed";
+// }
+// sayName()
+
+
+// function sayName() {
+//     var name ;
+//     console.log(name);
+//      name = "ahmed";
+    
+//   }
+//   sayName()
